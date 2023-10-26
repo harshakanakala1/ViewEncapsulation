b6c6ff0210281ee79eba8a7bf272af57abb2dfd9
harshakanakala1 <harshakanakala1@gmail.com>
2023-10-26 22:09:14 +0530

    View Encapsulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { Component1Component } from './component1/component1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentComponent,
+    Component1Component
   ],
   imports: [
     BrowserModule
